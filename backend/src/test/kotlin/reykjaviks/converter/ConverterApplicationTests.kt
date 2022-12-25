bea59eac1fed254bfc3c5aad8293aa6b03939897
Elizabeth <22527563+reykjaviks@users.noreply.github.com>
2022-12-25 20:56:57 +0200

    Init project

---
+++ backend/src/test/kotlin/reykjaviks/converter/ConverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package reykjaviks.converter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
